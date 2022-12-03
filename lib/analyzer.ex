b295748bb595e9fdc9592a4b31e6782113851d35
Leandro Cesquini Pereira <leandro@leandro.io>
2022-12-03 14:43:09 -0500

    Improve docs for normalize/2

--- lib/analyzer.ex
+++ lib/analyzer.ex
@@ -7,38 +7,106 @@ defmodule Analyzer do
       type: :string,
       required: false,
       default: "__analyzer_",
-      doc: "TODO"
+      doc: "Prefix for metadata keys."
     ]
   ]
 
   @doc """
   Transform `input` into a flat data structure.
-    
-  Rules are based on https://docs.airbyte.com/understanding-airbyte/basic-normalization
 
-  A few metadata columns will be injected into the resulting data:
-    
-    * hash_id - TODO
-    * normalized_at - TODO
+  ## Rules
 
-  They are prefixed with `__analyzer_` by default, but can be changed
-  by passing `:metadata_prefix` to `opts`.
+  A set of opinionated rules are applied to `input` in order to generate one or more
+  tabular data structures. The objective is to always generate Explorer's DataFrame compatible
+  data structures, so one can call `input |> Analyzer.normalize() |> Explorer.DataFrame.new()` but
+  the data is not limited to Explorer usage.
+    
+  Following is the list of rules applied to `input`, which are based on
+  https://docs.airbyte.com/understanding-airbyte/basic-normalization/#rules
+
+    * Inject `id` metadata key - a hash calculated for each record, it returns the same id given the same record.
+    * Inject `normalized_at` metadata key.
+    * Rename columns to snake_case - for better compatibily with 3rd-party tools.
+    * Convert keys to string - generate string keys for safety purposes.
+
+  The metadata keys `id` and `normalized_at` are injected with a prefix that
+  can be customized by passing `:metadata_prefix` to `opts`.
+
+  ## Examples
+
+      iex> Analyzer.normalize([%{"programmingLanguage" => "Elixir"}, %{"programmingLanguage" => "Rust"}])
+      [
+        %{
+          "__analyzer_id" => "Fy1fHQKmrxe29ABB",
+          "__analyzer_normalized_at" => ~U[2022-12-03 19:26:55.027477Z],
+          "programming_language" => "Elixir"
+        },
+        %{
+          "__analyzer_id" => "Fy1fHQKnriMiHQBh",
+          "__analyzer_normalized_at" => ~U[2022-12-03 19:26:55.027525Z],
+          "programming_language" => "Rust"
+        }
+      ]
+
+    Note that it always return a list, even if `input` is a single map, and that's due the fact that
+    a given input may have nested data that is break into multiple records injected with related foreign keys:
+    
+      iex> Analyzer.normalize(%{
+        "firstName" => "José",
+        "projects" => [
+          %{"name" => "elixir", "stars" => 21_000},
+          %{"name" => "livebook", "stars" => 3_000}
+        ]
+      })
+      [TODO]
+
+    And a single map:
+    
+      iex> Analyzer.normalize(%{"firstName" => "José"})
+      [
+        %{
+          "__analyzer_id" => "Fy1fsrwAcyZ2hQCh",
+          "__analyzer_normalized_at" => ~U[2022-12-03 19:37:38.096763Z],
+          "first_name" => "José"
+        }
+      ]
 
   Options:\n
     #{NimbleOptions.docs(@opts_normalize)}
   """
-  def normalize(input, opts \\ []) do
+  @spec normalize(input :: map() | [map()]) :: [map()]
+  def normalize(input, opts \\ [])
+
+  def normalize(input, opts) when is_list(input) do
+    Enum.map(input, fn i -> normalize(i, opts) end)
+  end
+
+  def normalize(input, opts) when is_map(input) do
     metadata_prefix_default = Kernel.get_in(@opts_normalize, [:metadata_prefix, :default])
     {metadata_prefix, _opts} = Keyword.pop(opts, :metadata_prefix, metadata_prefix_default)
 
-    hash_id = :todo
-    normalized_at = DateTime.utc_now()
+    input =
+      Map.new(input, fn {key, value} ->
+        {key, value}
+      end)
 
     metadata = %{
-      (metadata_prefix <> "hash_id") => hash_id,
-      (metadata_prefix <> "normalized_at") => normalized_at
+      (metadata_prefix <> "id") => hash_id(input),
+      (metadata_prefix <> "normalized_at") => DateTime.utc_now()
     }
 
-    Map.merge(input, metadata)
+    [Map.merge(input, metadata)]
+  end
+
+  @doc false
+  # https://github.com/phoenixframework/phoenix_live_view/blob/e80f0be89c9b67db0643067a29cb8c6cfccb5561/lib/phoenix_live_view/utils.ex#L470
+  def hash_id(term) do
+    binary = <<
+      System.system_time(:nanosecond)::64,
+      :erlang.phash2(term)::16,
+      :erlang.unique_integer()::16
+    >>
+
+    Base.url_encode64(binary)
   end
 end
--- test/analyzer_test.exs
+++ test/analyzer_test.exs
@@ -1,19 +1,29 @@
 defmodule AnalyzerTest do
   use ExUnit.Case
-  doctest Analyzer
 
-  describe "normalize" do
-    test "inject hash_id" do
-      assert %{"__analyzer_hash_id" => :todo} = Analyzer.normalize(%{foo: :bar})
+  test "hash_id" do
+    assert Analyzer.hash_id(%{foo: :bar}) |> is_binary()
+  end
+
+  describe "normalize metadata" do
+    test "inject id" do
+      assert [%{"__analyzer_id" => _}] = Analyzer.normalize(%{foo: :bar})
     end
 
     test "inject normalized_at" do
-      assert %{"__analyzer_normalized_at" => _} = Analyzer.normalize(%{foo: :bar})
+      assert [%{"__analyzer_normalized_at" => _}] = Analyzer.normalize(%{foo: :bar})
     end
 
     test "rename metadata prefix" do
-      assert %{"custom_normalized_at" => _} =
+      assert [%{"custom_normalized_at" => _}] =
                Analyzer.normalize(%{foo: :bar}, metadata_prefix: "custom_")
+    end
+  end
+
+  describe "normalize rules" do
+    @tag :skip
+    test "rename columns to snake_case" do
+      assert [%{"first_name" => "test"}] = Analyzer.normalize(%{"firstName" => "test"})
     end
   end
 end
