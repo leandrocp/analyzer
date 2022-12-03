236eaf2668687025673272c74ed14e83d7dfbc33
Leandro Cesquini Pereira <leandro@leandro.io>
2022-12-03 13:20:59 -0500

    Draft API normalize

--- lib/analyzer.ex
+++ lib/analyzer.ex
@@ -1,4 +1,44 @@
 defmodule Analyzer do
   @moduledoc """
   """
+
+  @opts_normalize [
+    metadata_prefix: [
+      type: :string,
+      required: false,
+      default: "__analyzer_",
+      doc: "TODO"
+    ]
+  ]
+
+  @doc """
+  Transform `input` into a flat data structure.
+    
+  Rules are based on https://docs.airbyte.com/understanding-airbyte/basic-normalization
+
+  A few metadata columns will be injected into the resulting data:
+    
+    * hash_id - TODO
+    * normalized_at - TODO
+
+  They are prefixed with `__analyzer_` by default, but can be changed
+  by passing `:metadata_prefix` to `opts`.
+
+  Options:\n
+    #{NimbleOptions.docs(@opts_normalize)}
+  """
+  def normalize(input, opts \\ []) do
+    metadata_prefix_default = Kernel.get_in(@opts_normalize, [:metadata_prefix, :default])
+    {metadata_prefix, _opts} = Keyword.pop(opts, :metadata_prefix, metadata_prefix_default)
+
+    hash_id = :todo
+    normalized_at = DateTime.utc_now()
+
+    metadata = %{
+      (metadata_prefix <> "hash_id") => hash_id,
+      (metadata_prefix <> "normalized_at") => normalized_at
+    }
+
+    Map.merge(input, metadata)
+  end
 end
--- test/analyzer_test.exs
+++ test/analyzer_test.exs
@@ -1,4 +1,19 @@
 defmodule AnalyzerTest do
   use ExUnit.Case
   doctest Analyzer
+
+  describe "normalize" do
+    test "inject hash_id" do
+      assert %{"__analyzer_hash_id" => :todo} = Analyzer.normalize(%{foo: :bar})
+    end
+
+    test "inject normalized_at" do
+      assert %{"__analyzer_normalized_at" => _} = Analyzer.normalize(%{foo: :bar})
+    end
+
+    test "rename metadata prefix" do
+      assert %{"custom_normalized_at" => _} =
+               Analyzer.normalize(%{foo: :bar}, metadata_prefix: "custom_")
+    end
+  end
 end
